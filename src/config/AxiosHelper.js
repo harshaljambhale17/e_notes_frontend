a08bdf6a38a0d215973ea0cb4f6d73f1e4ab667d
harshaljambhale17 <harshaljambhale17@gmail.com>
2025-11-05 23:59:27 +0530

    Update AxiosHelper.js
    Update the baseUrl

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = "http://localhost:8080"
+export const baseURL = process.env.REACT_APP_API_BASE_URL
 
 export const httpClient = axios.create({
     baseURL: baseURL,
